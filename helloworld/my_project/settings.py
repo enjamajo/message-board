9a10179b1771e653505381e527bc0d7b7b851aae
Jos de Vries <jos@enjamajoboekhouding.nl>
2023-09-10 09:37:27 +0200

    initial commit

--- helloworld/my_project/settings.py
+++ helloworld/my_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.webpages.apps.WebpagesConfig',  
+    'django.webpages.apps.WebpagesConfig',
 ]
 
 MIDDLEWARE = [
---
+++ website/pages/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Create your tests here.
+from django.test import SimpleTestCase
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_templtae_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Welcome to my website!</h1>")
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_templtae_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About Me</h1>")
