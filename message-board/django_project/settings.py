054c0503d7cef31781abb2a3abadc92f88cc4a66
Jos de Vries <jos@enjamajoboekhouding.nl>
2023-09-12 15:34:06 +0200

    New updates for Heroku deployment

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-04(zfi7qv2s2ne&c8=fbfm9z=##rd402fi#(75cu(*^xf@ji*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp", "localhost", "127.0.0.1"]
 
 
 # Application definition
